39187f92ed7db3428a19e6b620deff667b2bb359
MedicD21 <DSchaaf12@me.com>
2026-01-06 08:10:53 -0500

    folders

---
+++ MarsTime/Views/MarsTimeWatch Watch App/MarsTimeWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsTimeWatchApp.swift
+//  MarsTimeWatch Watch App
+//
+//  Created by Dustin Schaaf on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MarsTimeWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
