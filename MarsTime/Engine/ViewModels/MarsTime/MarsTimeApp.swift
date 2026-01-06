5c44676c3bd8782dd5122d8713fb9fd012885ed1
MedicD21 <DSchaaf12@me.com>
2026-01-06 08:08:40 -0500

    folder structure

---
+++ MarsTime/Engine/ViewModels/MarsTime/MarsTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsTimeApp.swift
+//  MarsTime
+//
+//  Created by Dustin Schaaf on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct MarsTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarsTime/MarsTimeWatch Watch AppTests/MarsTimeWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MarsTimeWatch_Watch_AppTests.swift
+//  MarsTimeWatch Watch AppTests
+//
+//  Created by Dustin Schaaf on 1/6/26.
+//
+
+import Testing
+@testable import MarsTimeWatch_Watch_App
+
+struct MarsTimeWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
